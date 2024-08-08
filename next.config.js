17b1a0edd5b6e9baeacb86295900f49d9efa3923
rlvv <rlvv@gft.com>
2024-08-08 14:32:12 -0300

    update config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: ".next",
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "**",
-        },
-      ],
-    },
-  }
-  
-  module.exports = nextConfig
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  env: {
+    ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
+    SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
+    SESSION_TOKEN: process.env.SESSION_TOKEN,
+  },
+};
+
+module.exports = nextConfig;
