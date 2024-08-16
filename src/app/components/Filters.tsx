ee3860b8388d933ae0b4ba448766c9e37543f453
rlvv <rlvv@gft.com>
2024-08-16 11:48:54 -0300

    removed '5' relevance

--- src/app/components/Filters.tsx
+++ src/app/components/Filters.tsx
@@ -54,7 +54,6 @@ const Filters: React.FC<FiltersProps> = ({ onFilterChange }) => {
             <option value="2">2</option>
             <option value="3">3</option>
             <option value="4">4</option>
-            <option value="5">5</option>
           </select>
         </div>
         <div>
